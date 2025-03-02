668e9a8a0356015239e96fb3879b870f26db0a68
Ishan Royyuru <142559396+ishanroyyuru@users.noreply.github.com>
2025-03-02 12:13:09 -0800

    Started home screen

--- Iris-Frontend/App.js
+++ Iris-Frontend/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Platypi: require("./assets/fonts/Platypi-Regular.ttf"),
+  });
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>
+        Iris
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: "#028960",
+    fontSize: 45,
+    fontWeight: "semibold",
+    position: "absolute",
+    top: "100",
+    left: "80",
+    fontFamily: "Platypi",
+  }
 });
+
