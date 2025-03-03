5dfc5d5b1fd91099520ea2ba51a34b6ed0952c0d
bxlyy <bryangly11@gmail.com>
2025-03-02 16:59:36 -0800

    added restaurants

--- Iris-Frontend/App.js
+++ Iris-Frontend/App.js
@@ -11,6 +11,18 @@ export default function App() {
   const [selectedCategory, setSelectedCategory] = useState("gym");
   const [bruinFitHours, setBruinFitHours] = useState("");
   const [JWCHours, setJWCHours] = useState("");
+  const [epicuriaHours, setEpicuriaHours] = useState("");
+  const [deNeveHours, setDeNeveHours] = useState("");
+  const [feastHours, setFeastHours] = useState("");
+  const [bPlateHours, setBPlateHours] = useState("");
+  const [bCafeHours, setBCafeHours] = useState("");
+  const [cafe1919Hours, setCafe1919Hours] = useState("");
+  const [rendeHours, setRendeHours] = useState("");
+  const [studyHours, setStudyHours] = useState("");
+  const [dreyHours, setDreyHours] = useState("");
+  const [epicAckermanHours, setEpicAckermanHours] = useState("");
+  const [rieberTrucksHours, setRieberTrucksHours] = useState("");
+  const [sproulTrucksHours, setSproulTrucksHours] = useState("");
 
   // Fetch the data from the backend API
   const fetchBruinFit = async () => {
@@ -49,21 +61,256 @@ export default function App() {
     }
   };
 
+  const fetchEpicuria = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/epicuria');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setEpicuriaHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchDeNeve = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/deNeve');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setDeNeveHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchFeast = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/feast');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setFeastHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchBPlate = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/bPlate');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setBPlateHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchBCafe = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/bCafe');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setBCafeHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchCafe1919 = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/cafe1919');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setDeNeveHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchRende = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/rende');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setRendeHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchStudy = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/study');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setStudyHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchDrey = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/drey');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setDreyHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchEpicAckerman = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/epicAckerman');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setEpicAckermanHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchRieberTrucks = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/rieberTrucks');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setRieberTrucksHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+  const fetchSproulTrucks = async () => {
+    try {
+      // Change localhost to the appropriate IP for Android emulators
+      const response = await fetch('http://127.0.0.1:5001/api/facility/sproulTrucks');  // For Android
+      // For iOS, use 'localhost:5000' or 'http://127.0.0.1:5001'
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Data:', data);  // Log the data to check its structure
+      setSproulTrucksHours(data.data);  // Assuming the backend sends the data as { data: '...' }
+    } catch (error) {
+      console.error('Error fetching facilities:', error.message || error);
+    }
+  };
+
+
   // Calling fetchBruinFit once the component mounts
   useEffect(() => {
     fetchBruinFit();
     fetchJWC();
+    fetchEpicuria();
+    fetchDeNeve();
+    fetchFeast();
+    fetchBPlate();
+    fetchBCafe();
+    fetchCafe1919();
+    fetchRende();
+    fetchStudy();
+    fetchDrey();
   }, []);
 
   // Hardcoded facility data based on selection
   const facilities = {
     gym: [
-      { name: "Bruin Fit", hours: "8:00 AM - 8:00 PM" },
-      { name: "John Wooden", hours: "6:00 AM - 10:00 PM" }
+      { name: "Bruin Fit", hours: bruinFitHours },
+      { name: "John Wooden", hours: JWCHours }
     ],
     dining: [
-      { name: "De Neve", hours: "9:00 AM - 5:00 PM" },
-      { name: "Rendezvous", hours: "11:00 AM - 9:00 PM" }
+      { name: "De Neve", hours: deNeveHours },
+      { name: "Feast", hours: feastHours },
+      { name: "Bruin Plate", hours: bPlateHours },
+      { name: "Bruin Cafe", hours: bCafeHours },
+      { name: "Cafe 1919", hours: cafe1919Hours },
+      { name: "Rendezvous", hours: rendeHours },
+      { name: "The Study at Hedrick", hours: studyHours },
+      { name: "The Drey", hours: dreyHours },
+      { name: "Epicuria at Ackerman", hours: epicAckermanHours },
+      { name: "Rieber Food Trucks", hours: rieberTrucksHours },
+      { name: "Sproul Food Trucks", hours: sproulTrucksHours }
     ]
   };
 
